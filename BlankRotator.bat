2c3e88fc75ae98c863ff6f2a45341544fa94e42c
dripbilliam <gxlyplays@gmail.com>
2026-05-16 12:02:25 -0500

    Now accounts for extended nwclientLogx files for those really long days

--- BlankRotator.bat
+++ BlankRotator.bat
@@ -1,69 +1,79 @@
 @echo off
 setlocal EnableDelayedExpansion
- 
+
 echo *** Starting Neverwinter Nights ***
- 
+
 :: BOTH WITHOUT QUOTES
 :: Your NWN:EE document directory on the next line, the location of your logs directory and nwnplayer.ini
-set "EEDocDir=NWN DOCUMENTS FOLDER HERE
+set "EEDocDir=NWN DOCUMENTS FOLDER HERE"
 :: Your EE nwmain.exe install directory on the next line
-set "EEInstallDir=STEAMAPPS NWN WIN32 DIRECTORY HERE
- 
+set "EEInstallDir=STEAMAPPS NWN WIN32 DIRECTORY HERE"
+
 set "logdir=%EEDocDir%"
- 
- 
- 
- 
- 
- 
+
+
 :: --- PRE-LAUNCH LOG ARCHIVE ---
-if exist "%logdir%\logs\nwclientLog1.txt" (
-    echo Found existing log before launch, archiving...
-    call :ArchiveLog
+if exist "%logdir%\logs\nwclientLog*.txt" (
+    echo Found existing logs before launch, archiving...
+    call :ArchiveAllLogs
 )
- 
+
 START "" /WAIT steam://rungameid/704450
- 
+
 timeout /t 5 /nobreak > nul
- 
+
 :loop
 timeout /t 1 /nobreak > nul
 tasklist /fi "imagename eq nwmain.exe" | find ":" > nul
 if errorlevel 1 goto loop
- 
+
 echo *** Neverwinter Nights Terminated ***
 echo *** Processing Logs ***
- 
+
 :: --- POST-EXIT LOG ARCHIVE ---
-if exist "%logdir%\logs\nwclientLog1.txt" (
-    call :ArchiveLog
+if exist "%logdir%\logs\nwclientLog*.txt" (
+    call :ArchiveAllLogs
 )
- 
+
 cd /D "%EEInstallDir%"
 ".\Neverwinter Nights Enhanced Edition.url"
 goto :eof
- 
- 
+
+
+:ArchiveAllLogs
+    for %%F in ("%logdir%\logs\nwclientLog*.txt") do (
+        if exist "%%~fF" (
+            call :ArchiveLog "%%~fF"
+        )
+    )
+goto :eof
+
+
 :ArchiveLog
+    set "src=%~1"
+
     :: get timestamp
     for /f %%i in ('powershell -NoProfile -Command "Get-Date -Format yyyy-MM-dd_HH-mm-ss"') do set "datetime=%%i"
     set "TODAY=!datetime:~0,10!"
     set "NOW=!datetime:~11!"
- 
+
+    :: keep original log number/name in the archived filename
+    set "srcname=%~n1"
+
     :: base path without extension
-    set "base=%logdir%\logs\log-!TODAY!-!NOW!"
+    set "base=%logdir%\logs\log-!TODAY!-!NOW!-!srcname!"
     set "target=!base!.txt"
     set /a n=1
- 
-    :: if a file with this name already exists, append fixture, ...
+
+    :: if a file with this name already exists, append _1, _2, etc.
     :check_target
     if exist "!target!" (
         set "target=!base!_!n!.txt"
         set /a n+=1
         goto check_target
     )
- 
-    echo Archiving "%logdir%\logs\nwclientLog1.txt"
-    echo   -> "!target!"
-    move "%logdir%\logs\nwclientLog1.txt" "!target!" >nul
+
+    echo Archiving "!src!"
+    echo   -^> "!target!"
+    move "!src!" "!target!" >nul
 goto :eof
